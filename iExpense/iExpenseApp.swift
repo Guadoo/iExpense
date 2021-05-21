7cb0fcf4013eb39c9132c7e0eb30cbe9256d69f4
Liu Jingkai <rock_jk@msn.com>
2021-05-21 15:26:27 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Guadoo on 2021/5/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
